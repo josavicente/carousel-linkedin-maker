01f684c510ee83666d4d5d18064b47fbf98daca7
Josa Vicente <jvicenpe@gmail.com>
2023-01-01 21:35:07 +0100

    V1.1 Playing with strings

--- carousel-linkedin.maker.py
+++ carousel-linkedin.maker.py
@@ -6,54 +6,64 @@ from PyPDF2 import PdfMerger
 CONFIG_FILE_NAME = "config-carousel-linkedin-maker.json"
 ORIGIN_FOLDER_FIELD = "origin_folder"
 FINAL_FOLDER_FIELD = "final_folder"
-    
+
 class CarouselLinkedinMaker():
-    
-    configFileData = ""
+    """# Automatization of merging PDF's from a input folder"""
+    config_file_data = ""
     input_folder = ""
     output_folder = ""
-    
+
     def __init__(self):
         # super(CarouselLinkedinMaker, self).__init__("josavicentedev")
-        self.openConfigDataFile()
-        self.checkFolderInput()
-        self.checkFolderOutput()
-        self.mergePDFs()
-        
-    def openConfigDataFile(self):
-        configFile = open(CONFIG_FILE_NAME)
-        self.configFileData = json.load(configFile)
-    
-    def checkFolderInput(self):
-        self.input_folder = self.checkFolder(self.configFileData[ORIGIN_FOLDER_FIELD])
-        
-    def checkFolderOutput(self):
-        self.output_folder =  self.checkFolder(self.configFileData[FINAL_FOLDER_FIELD])
-        
-    
-    def checkFolder(self, folder_name):
-        CHECK_FOLDER = os.path.isdir(folder_name)
-        # If folder doesn't exist, then create it.
-        if not CHECK_FOLDER:
+        self.open_config_data_file()
+        self.check_folder_input()
+        self.check_folder_output()
+        self.merge_pdfs()
+
+    def open_config_data_file(self):
+        """# Open config data"""
+        config_file = open(CONFIG_FILE_NAME)
+        self.config_file_data = json.load(config_file)
+
+    def check_folder_input(self):
+        """# Checking existence of input folder and/or create"""
+        self.input_folder = self.check_folfer(self.config_file_data[ORIGIN_FOLDER_FIELD])
+
+    def check_folder_output(self):
+        """# Checking existence of output folder and/or create"""
+        self.output_folder =  self.check_folfer(self.config_file_data[FINAL_FOLDER_FIELD])
+
+    def check_folfer(self, folder_name):
+        """# Checking existence fo given folder and/or create"""
+        check_folder = os.path.isdir(folder_name)
+        if not check_folder:
             os.makedirs(folder_name)
             print("created folder : ", folder_name)
-
         else:
             print(folder_name, "folder already exists.")
             return folder_name
-        
-    def mergePDFs(self):
+
+    def merge_pdfs(self):
+        """# Merge PDF's"""
         print(self.input_folder)
-        listOfPdfs = [f for f in glob.glob(self.input_folder + "/*.pdf")]
-        listOfPdfs.sort
-        print(listOfPdfs)
+        list_of_pfds = [f for f in glob.glob(self.input_folder + "/*.pdf")]
+        list_of_pfds.sort()
+        print(list_of_pfds)
+        self.group_by_name(list_of_pfds)
         merger = PdfMerger()
 
-        for pdf in listOfPdfs:
+        for pdf in list_of_pfds:
             merger.append(pdf)
 
         merger.write(self.output_folder + "/result.pdf")
         merger.close()
-
+    def group_by_name(self, list_of_pdfs):
+        # A list comprehension. It is a way to create a list from another list.
+        list_splitted = [element.lower().replace('/', ' ').replace('.', ' ').split() for element in list_of_pdfs]
+        print(list_splitted)
+        file_name_set = set()
+        for pdf in list_splitted:
+            file_name = ''.join((x for x in pdf[1] if not x.isdigit()))
+            file_name_set.add(file_name)
 if __name__ == "__main__":
     CarouselLinkedinMaker()
